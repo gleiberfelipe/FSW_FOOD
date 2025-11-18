344bb7fc5ae7f2c4bb28e50d35c0b2980b33ebe4
gleiberfelipe <gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.com>
2025-11-18 01:31:36 -0300

    feat: add Header and Search components with input functionality

---
+++ app/_components/ui/search.tsx
@@ -0,0 +1,16 @@
+import { Input } from "@/app/_lib/ui/input";
+import { Button } from "@/app/_lib/ui/button";
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+  return (
+    <div className="flex gap-2">
+      <Input />
+      <Button size="icon">
+        <SearchIcon size={18} />
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
