ccbf32917fddefa83aab2595b28635661737a56c
gleiberfelipe <gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.comgit config --global user.email gleiberfelipe@gmail.com>
2025-11-18 23:59:09 -0300

    feat:: add init search component

--- app/_components/ui/search.tsx
+++ app/_components/ui/search.tsx
@@ -5,7 +5,7 @@ import { SearchIcon } from "lucide-react";
 const Search = () => {
   return (
     <div className="flex gap-2">
-      <Input />
+      <Input placeholder="Buscar restaurantes" className="border-none" />
       <Button size="icon">
         <SearchIcon size={18} />
       </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import Header from "./_components/ui/headers";
 import Search from "./_components/ui/search";
 
 const Home = () => {
-  return;
-  <>
-    <Header />
-    <Search />
-  </>;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 };
 
 export default Home;
